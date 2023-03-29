ad3a482d61f3861b2e6e0f38e756fed6b6894adf
youssef-study <youssef-study23@gmail.com>
2023-03-28 18:34:28 -0700

    TASK1-2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the values of two integers.
+ * @a: Pointer to an int.
+ * @b: Pointer to an int.
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - length of string
+ * Description: this is task 2
+ * @s: pointer to char
+ * Return: i
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
